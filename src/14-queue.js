fc50bcf2dce2b2fcc8c8f67e45a873dcc6298b0c
alexpodelinskii <nikitik007@yandex.ru>
2021-04-13 10:34:35 +0300

    Update 14-queue.js

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
+/ * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
  * @example
  * const queue = new Queue();
